dddc939fbc13d73f0fb9d7449ec6ea3a14de6702
wxlcome <814347212@qq.com>
2024-12-05 13:13:46 +0800

    修改bug

--- model/src/main/java/com/wxl/cloud/miniecommerce/model/entity/system/OperationGoods.java
+++ model/src/main/java/com/wxl/cloud/miniecommerce/model/entity/system/OperationGoods.java
@@ -1,15 +1,17 @@
 package com.wxl.cloud.miniecommerce.model.entity.system;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.wxl.cloud.miniecommerce.model.entity.BaseEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @ClassName  ：OperationGoods
